5944d98c3809e3fef36d42398afd837108857185
chikap421 <chikap421@gmail.com>
2024-08-03 12:54:34 +0100

    First 20 problems

---
+++ 274. H-Index.py
@@ -0,0 +1,49 @@
+# O(n) time complexity solution
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        n = len(citations)
+        count = [0] * (n + 1)
+        
+        # Populate the count array
+        for c in citations:
+            if c >= n:
+                count[n] += 1
+            else:
+                count[c] += 1
+        
+        total = 0
+        # Traverse the count array from end to start
+        for i in range(n, -1, -1):
+            total += count[i]
+            if total >= i:
+                return i
+        
+        return 0
+
+
+# O(nlogn) time complexity solution 
+# class Solution(object):
+#     def hIndex(self, citations):
+#         """
+#         :type citations: List[int]
+#         :rtype: int
+#         """
+        
+#         # Sort the citations in the descending order
+#         citations.sort(reverse=True)
+
+#         h_index = 0
+
+#         # Iterate through the sorted list and find the h-index
+#         for i in range (len(citations)):
+#             if citations[i] >= i + 1:
+#                 h_index = i + 1
+#             else:
+#                 break
+#         return h_index
+
+
